3c36ec3e5ea0fa13087fc9474a18d0b6d1d230b0
Chris Ragland <Chrisragland97@gmail.com>
2022-12-17 05:51:15 -0500

    rm Logo.jsx rm Instructions.jsx edit Navigation rm blob.jsx page edit index.jsx page

--- src/components/dom/Navigation.jsx
+++ src/components/dom/Navigation.jsx
@@ -1,9 +1,9 @@
 export default function Navigation() {
   return (
     <nav className='flex justify-around w-screen py-4'>
-      <div className='order-1'>D</div>
-      <div className='order-2'>LOGO</div>
-      <div className='order-3'>Contact</div>
+      <div className='order-1'>CR</div>
+      <div className='order-2'>Contact</div>
+      <div className='order-3'>D</div>
     </nav>
   )
 }
