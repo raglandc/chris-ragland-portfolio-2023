f86f2492e608c67df6158dfc88fea1df5df3baf4
Chris Ragland <Chrisragland97@gmail.com>
2022-12-18 17:57:18 -0500

    replace hero-icons with react-icons

--- src/components/dom/Navigation.jsx
+++ src/components/dom/Navigation.jsx
@@ -1,25 +1,11 @@
-import { useState } from 'react'
 import ContactButton from '../util/ContactButton'
-import { SunIcon, MoonIcon } from '@heroicons/react/24/outline'
 
 export default function Navigation() {
-  const [lightMode, setLightMode] = useState(true)
-
-  const toggleLightMode = () => {
-    setLightMode((prev) => !prev)
-  }
-
   return (
     <nav className='z-10 flex items-center justify-between w-screen h-20 px-6'>
-      <div className='order-1'>CR</div>
+      <div className='w-2/4'>CR</div>
 
-      <div className='flex flex-row order-2 w-max'>
-        <div
-          onClick={toggleLightMode}
-          className='flex items-center mx-2 rounded-full hover:text-primary hover:cursor-pointer'
-        >
-          {lightMode ? <MoonIcon className='w-5 h-5' /> : <SunIcon className='w-5 h-5' />}
-        </div>
+      <div className='flex flex-row justify-end w-2/4'>
         <ContactButton />
       </div>
     </nav>
