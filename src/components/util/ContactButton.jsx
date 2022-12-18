50ad64ae8539fc57bbadd0218b0d1d6a9ab83a73
Chris Ragland <Chrisragland97@gmail.com>
2022-12-18 18:00:07 -0500

    removing padding and margin no longer needed on contact button

--- src/components/util/ContactButton.jsx
+++ src/components/util/ContactButton.jsx
@@ -1,3 +1,3 @@
 export default function ContactButton() {
-  return <button className='px-4 py-1 mx-1 text-textPrimary rounded-md hover:text-primary h-max'>Contact</button>
+  return <button className='text-textPrimary rounded-md hover:text-primary h-max'>Contact</button>
 }
