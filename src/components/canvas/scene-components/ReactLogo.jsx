b51b154d4ff8343daf6c508c22b35c7555629120
Chris Ragland <Chrisragland97@gmail.com>
2022-12-20 08:42:48 -0500

    create and position react logo in scene two

--- src/components/canvas/scene-components/ReactLogo.jsx
+++ src/components/canvas/scene-components/ReactLogo.jsx
@@ -1,20 +1,17 @@
-import { useControls } from 'leva'
 import { useGLTF } from '@react-three/drei'
-
-export default function CoffeeCup(props) {
+import { useControls } from 'leva'
+export default function ReactLogo(props) {
   // const { x, y, z } = useControls({
   //   x: { value: -1.8, min: -10, max: 10, step: 0.01 },
   //   y: { value: 0, min: -10, max: 10, step: 0.01 },
   //   z: { value: -1.0, min: -10, max: 10, step: 0.01 },
   // })
-
   const { scene } = useGLTF(
-    'https://market-assets.fra1.cdn.digitaloceanspaces.com/market-assets/models/spilling-coffee/model.gltf',
+    'https://market-assets.fra1.cdn.digitaloceanspaces.com/market-assets/models/react-logo/model.gltf',
   )
   return (
     <primitive
-      rotation={[0, -0.6, -0.25]}
-      position={[-1.7, 1, -1.0]}
+      position={[1.5, -3.4, -1.0]}
       object={scene}
       {...props}
     />
--- src/components/canvas/scenes/SceneTwo.jsx
+++ src/components/canvas/scenes/SceneTwo.jsx
@@ -2,6 +2,7 @@ import { useGLTF, Float } from '@react-three/drei'
 import dynamic from 'next/dynamic'
 
 const CoffeeCup = dynamic(() => import('@/components/canvas/scene-components/CoffeeCup.jsx'))
+const ReactLogo = dynamic(() => import('@/components/canvas/scene-components/ReactLogo.jsx'))
 
 export default function SceneTwo(props) {
   return (
@@ -10,6 +11,7 @@ export default function SceneTwo(props) {
       speed={0.5}
     >
       <CoffeeCup />
+      <ReactLogo />
     </Float>
   )
 }
