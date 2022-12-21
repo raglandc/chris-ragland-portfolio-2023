828902f108b97793d2a7a346c9e1b3c8ef817a8e
Chris Ragland <Chrisragland97@gmail.com>
2022-12-21 10:17:46 -0500

    footer styles

--- src/components/dom/Footer.jsx
+++ src/components/dom/Footer.jsx
@@ -1,6 +1,5 @@
 export default function Footer() {
   const year = new Date().getFullYear()
-  console.log(year)
 
   return (
     <footer className='flex flex-col items-center justify-center order-last h-40 text-white bg-black col-span-full'>
