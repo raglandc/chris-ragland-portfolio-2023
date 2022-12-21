b666e53082ce6160d46cf22c715812b3f0d434c3
Chris Ragland <Chrisragland97@gmail.com>
2022-12-21 07:21:20 -0500

    style work for navigation

--- src/components/dom/ContactButton.jsx
+++ src/components/dom/ContactButton.jsx
@@ -1,3 +1,39 @@
-export default function ContactButton() {
-  return <button className='rounded-md hover:text-primary h-max'>Contact</button>
+import { useState } from 'react'
+export default function ContactButton(props) {
+  const [showContact, setShowContact] = useState(false)
+
+  const showContactHandler = () => {
+    setShowContact((prev) => !prev)
+  }
+
+  return (
+    <>
+      <button
+        onClick={showContactHandler}
+        className='px-2 py-1 text-white shadow-md active:shadow-sm rounded-md hover:bg-secondary bg-primary h-max'
+      >
+        Contact
+      </button>
+
+      {showContact && (
+        <ContactWindow
+          closeHandler={showContactHandler}
+          layoutRef={props.layoutRef}
+        />
+      )}
+    </>
+  )
+}
+
+function ContactWindow(props) {
+  return (
+    <div className='absolute top-0 left-0 w-full h-full bg-black/80'>
+      <div className='w-2/4'>
+        <div className='flex justify-around w-full'>
+          <h1>Say Hi 👋</h1>
+          <p onClick={props.closeHandler}> close [x] </p>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/dom/Navigation.jsx
+++ src/components/dom/Navigation.jsx
@@ -1,12 +1,12 @@
 import ContactButton from './ContactButton'
 
-export default function Navigation() {
+export default function Navigation(props) {
   return (
-    <nav className='fixed z-10 flex items-center justify-between w-screen px-6 h-14 bg-white/20 backdrop-blur-lg drop-shadow-md'>
+    <nav className='fixed z-10 flex items-center justify-between w-screen px-6 shadow-md h-14 bg-white/50 backdrop-blur-lg drop-shadow-md'>
       <div className='w-2/4'>CR</div>
 
       <div className='flex flex-row justify-end w-2/4'>
-        <ContactButton />
+        <ContactButton layoutRef={props.layoutRef} />
       </div>
     </nav>
   )
