23d95f3e9838a6de102b4bcb41a58403ea828326
Chris Ragland <Chrisragland97@gmail.com>
2022-12-21 09:51:11 -0500

    design and add footer content

--- src/components/dom/Footer.jsx
+++ src/components/dom/Footer.jsx
@@ -1,3 +1,11 @@
 export default function Footer() {
-  return <footer className='order-last h-40 text-white bg-black col-span-full'>This is the footer</footer>
+  const year = new Date().getFullYear()
+  console.log(year)
+
+  return (
+    <footer className='flex flex-col items-center justify-center order-last h-40 text-white bg-black col-span-full'>
+      <p> Made With ❤️ by Chris Ragland </p>
+      <p>&copy; {year} </p>
+    </footer>
+  )
 }
