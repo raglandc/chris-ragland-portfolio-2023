98ae18ebf3d5dd88917f53a437f22f6eed459ec4
Chris Ragland <Chrisragland97@gmail.com>
2022-12-23 18:13:29 -0500

    new menu bar

--- src/components/dom/Navigation.jsx
+++ src/components/dom/Navigation.jsx
@@ -2,11 +2,13 @@ import Contact from './Contact'
 
 export default function Navigation(props) {
   return (
-    <nav className='fixed z-10 flex items-center justify-between w-screen px-6 shadow-md h-14 bg-white/50 backdrop-blur-lg drop-shadow-md'>
-      <div className='w-2/4'>CR</div>
+    <nav className='fixed z-10 flex justify-center w-screen p-4 items center h-max'>
+      <div className='flex items-center justify-between w-3/4 px-4 border rounded-lg shadow-md border-white/50 bg-white/20 backdrop-blur-lg drop-shadow-md h-14'>
+        <div className='w-2/4'>CR</div>
 
-      <div className='flex flex-row justify-end w-2/4'>
-        <Contact />
+        <div className='flex flex-row justify-end w-2/4'>
+          <Contact />
+        </div>
       </div>
     </nav>
   )
