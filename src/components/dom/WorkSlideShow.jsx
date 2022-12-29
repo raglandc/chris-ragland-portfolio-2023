f207981b8a099a28c7551bd650e0a752e232c8dc
Chris Ragland <Chrisragland97@gmail.com>
2022-12-29 11:36:56 -0500

    window pop up work looks great

--- src/components/dom/WorkSlideShow.jsx
+++ src/components/dom/WorkSlideShow.jsx
@@ -4,6 +4,20 @@ import { motion } from 'framer-motion'
 import PortalOverlay from '../util/PortalOverlay'
 import PortalBackdrop from '../util/PortalBackdrop'
 import { VscClose } from 'react-icons/vsc'
+import {
+  SiJavascript,
+  SiReact,
+  SiTypescript,
+  SiBlender,
+  SiCplusplus,
+  SiC,
+  SiNextdotjs,
+  SiSvelte,
+  SiThreedotjs,
+  SiHtml5,
+  SiCss3,
+  SiMongodb,
+} from 'react-icons/si'
 
 export default function WorkSlideShow() {
   return (
@@ -16,13 +30,14 @@ export default function WorkSlideShow() {
           image={item.image}
           projectType={item.projectType}
           description={item.description}
+          skills={item.skills}
         />
       ))}
     </div>
   )
 }
 
-function Slide({ title, link, description, image, projectType }) {
+function Slide({ title, link, description, image, projectType, skills }) {
   const [showPopUp, setShowPopUp] = useState(false)
   const showPopUpHandler = () => {
     setShowPopUp((prev) => !prev)
@@ -63,6 +78,7 @@ function Slide({ title, link, description, image, projectType }) {
             image={image}
             link={link}
             description={description}
+            skills={skills}
           />
         </>
       )}
@@ -70,13 +86,34 @@ function Slide({ title, link, description, image, projectType }) {
   )
 }
 
-function SlidePopUp({ title, link, image, projectType, description, closeHandler }) {
+function SlidePopUp({ title, link, image, projectType, description, closeHandler, skills }) {
   return (
-    <PortalOverlay className='fixed z-30 flex justify-center w-max -translate-y-2/4 -translate-x-2/4 top-2/4 left-2/4 h-max text-slate-100'>
-      <div className='w-10/12 bg-slate-800'>
-        <div className='flex items-center justify-center'>
-          <h2>{title}</h2>
-          <VscClose />
+    <PortalOverlay className='fixed z-30 flex justify-center w-full -translate-y-2/4 -translate-x-2/4 top-2/4 left-2/4 h-max text-slate-100'>
+      <div className='w-10/12 rounded-lg bg-slate-800'>
+        <div className='flex items-center justify-between px-6 py-4'>
+          <h1 className='text-4xl font-bold'>{title}</h1>
+          <VscClose
+            className='p-1 cursor-pointer w-max h-max hover:rounded-full hover:bg-slate-900/30'
+            onClick={closeHandler}
+          />
+        </div>
+        <div className='flex flex-col items-center w-full'>
+          <div className='relative w-10/12 h-48'>
+            <Image
+              src={image}
+              alt={`${title} website`}
+              layout='fill'
+              objectFit='cover'
+              style={{ borderRadius: '.5rem' }}
+            />
+          </div>
+          <p className='w-10/12 py-4'>{description}</p>
+          <p className='w-10/12 pt-3 text-center border-t'>Project Skills</p>
+          <div className='flex items-center justify-around w-10/12 py-4 mb-3 text-xl border-b'>
+            {skills.map((item, index) => (
+              <span key={index}>{item}</span>
+            ))}
+          </div>
         </div>
       </div>
     </PortalOverlay>
@@ -91,6 +128,12 @@ const workArray = [
     projectType: 'Web Development',
     description:
       'Vidal Developers is a freelance web development business Chris started when he began learning web development.',
+    skills: [
+      <SiTypescript key={'Typescript Logo'} />,
+      <SiReact key={'React Logo'} />,
+      <SiNextdotjs key={'NextJS Logo'} />,
+      <SiCss3 key={'Css3 Logo'} />,
+    ],
   },
   {
     title: 'The Daily Derivative',
