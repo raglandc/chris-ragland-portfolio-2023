fca452159902639e13c93b1b1b8c75f0c9fa6f6b
Chris Ragland <Chrisragland97@gmail.com>
2022-12-30 11:52:33 -0500

    interactive work

--- src/components/canvas/SceneContainer.jsx
+++ src/components/canvas/SceneContainer.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload } from '@react-three/drei'
+import { OrbitControls, Environment, Preload, AccumulativeShadows, RandomizedLight } from '@react-three/drei'
 import PortalBackdrop from '@/components/util/PortalBackdrop'
 import SceneOne from './scenes/SceneOne'
 
@@ -14,12 +14,39 @@ export default function SceneContainer({ children, ...props }) {
   return (
     <>
       <Canvas
-        {...props}
+        shadows
+        camera={{ position: [0, 1.5, 8], fov: 45 }}
         className='absolute top-0 left-0 z-10 w-full h-full'
       >
-        <OrbitControls />
-        <ambientLight intensity={1} />
-        <directionalLight intensity={1} />
+        <OrbitControls
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          enableZoom={false}
+        />
+        <Environment preset='city' />
+        <AccumulativeShadows
+          temporal
+          frames={100}
+          color='orange'
+          colorBlend={2}
+          toneMapped={true}
+          alphaTest={0.9}
+          opacity={2}
+          scale={12}
+        >
+          <RandomizedLight
+            amount={8}
+            radius={4}
+            ambient={0.5}
+            intensity={1}
+            position={[5, 5, -10]}
+            bias={0.001}
+          />
+        </AccumulativeShadows>
+        {/* <directionalLight
+          intensity={1}
+          castShadow
+        /> */}
         <SceneOne showStateHandler={showStateHandler} />
         <Preload all />
       </Canvas>
--- src/components/canvas/scenes/SceneOne.jsx
+++ src/components/canvas/scenes/SceneOne.jsx
@@ -1,6 +1,8 @@
 import { useRef } from 'react'
+import * as THREE from 'three'
 import { useGLTF, Html } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
+import { useControls } from 'leva'
 
 export default function SceneOne({ showState, showStateHandler }) {
   const planeRef = useRef()
@@ -16,19 +18,54 @@ export default function SceneOne({ showState, showStateHandler }) {
   const pinTwo = useGLTF('/models/pin-two.glb')
 
   return (
-    <group rotation={[Math.PI * 0.13, 0.41, 0]}>
-      <primitive object={world.scene} />
-      {/* pins on the earth : Florida and Indiana */}
-      <primitive
-        onClick={showStateHandler}
-        object={pinOne.scene}
-      />
-      <primitive object={pinTwo.scene} />
+    <>
+      <group rotation={[Math.PI * 0.13, 0.41, 0]}>
+        <primitive
+          object={world.scene}
+          castShadow
+          receiveShadow
+        />
+        {/* pins on the earth : Florida and Indiana */}
+        <primitive
+          onClick={showStateHandler}
+          object={pinOne.scene}
+        />
+        <primitive object={pinTwo.scene} />
 
-      <primitive
-        ref={planeRef}
-        object={plane.scene}
-      />
-    </group>
+        <primitive
+          castShadow
+          ref={planeRef}
+          object={plane.scene}
+        />
+      </group>
+      {/* <ShadowCatcher /> */}
+    </>
   )
 }
+
+// function ShadowCatcher() {
+//   const { rx, py, rz } = useControls({
+//     rx: {
+//       value: -Math.PI / 2,
+//       min: -Math.PI * 2,
+//       max: Math.PI * 2,
+//       step: 0.01,
+//     },
+//     py: {
+//       value: -3.45,
+//       min: -10,
+//       max: 0,
+//       step: 0.01,
+//     },
+//   })
+//   return (
+//     <mesh
+//       rotation={[rx, 0, 0]}
+//       position={[0, py, 0]}
+//       receiveShadow
+//     >
+//       <planeGeometry args={[100, 100]} />
+//       <meshStandardMaterial />
+//     </mesh>
+//   )
+// }
