b2c4ae915ff79fd6feb8cf94fb5f49e0a9efca98
Chris Ragland <Chrisragland97@gmail.com>
2023-01-06 07:11:57 -0500

    stye fix for footer

--- src/components/dom/Footer.jsx
+++ src/components/dom/Footer.jsx
@@ -2,9 +2,9 @@ export default function Footer() {
   const year = new Date().getFullYear()
 
   return (
-    <footer className='flex flex-col items-center justify-center order-last text-white h-60 from-black bg-gradient-to-t col-span-full'>
-      <p> Made With ❤️ by Chris Ragland </p>
-      <p>&copy; {year} </p>
+    <footer className='flex flex-col items-center justify-center order-last w-full h-64 text-slate-100 from-black bg-gradient-to-t'>
+      <p className='mt-5'> Made With ❤️ by Chris Ragland </p>
+      <p className='mt-5'>&copy; {year} </p>
       <p className='px-5 mt-5 text-sm text-center text-slate-500'>
         A special thanks to Sriniwasjha, Pozo3d, Alex Sanches, and Ole Gunnar Isager for letting me use their beautiful
         art work for some of this project.
