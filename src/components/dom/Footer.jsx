88e2a91c42191455fea64c8e503ae6c9ad2ab383
Chris Ragland <Chrisragland97@gmail.com>
2023-03-10 17:55:27 -0500

    remove footer p tag

--- src/components/dom/Footer.jsx
+++ src/components/dom/Footer.jsx
@@ -5,10 +5,6 @@ export default function Footer() {
     <footer className='flex flex-col items-center justify-center order-last w-full h-64 text-slate-100 from-black bg-gradient-to-t'>
       <p className='mt-5'> Made With ❤️ by Chris Ragland </p>
       <p className='mt-5'>&copy; {year} </p>
-      <p className='px-5 mt-5 text-sm text-center text-slate-500'>
-        A special thanks to Sriniwasjha, Pozo3d, Alex Sanches, and Ole Gunnar Isager for letting me use their beautiful
-        art work for some of this project.
-      </p>
     </footer>
   )
 }
