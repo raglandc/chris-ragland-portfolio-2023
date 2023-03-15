5eced95f171e8668ca991ba016cdcc5019babf64
Chris Ragland <Chrisragland97@gmail.com>
2023-03-15 18:34:36 -0400

    aspect ratio of slide show fix

--- src/components/dom/SlideShow.jsx
+++ src/components/dom/SlideShow.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect, useRef } from "react"
 import Image from "next/image";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs"
 
 //array of photos
 const photos = [
   {
-    photoLink: '/img/work-images/daily-derivative-landing-page.webp',
-    description: "test 1"
+    photoLink: '/img/life-images/cda-lab.webp',
+    description: "cda lab circuit"
   },
   {
     photoLink: '/img/work-images/vidal-developers-landing-page.webp',
@@ -50,7 +51,7 @@ export default function SlideShow()
   }, [isPaused]);
 
   return (
-  <div className="w-10/12 m-auto">
+  <div className="flex flex-col items-center justify-center w-full h-full md:w-11/12 lg:w-10/12 xl:w-9/12 2xl:w-8/12">
     <h1 className='w-11/12 mb-10 text-2xl sm:text-4xl'>
           <span className='font-bold text-left text-transparent animate-text bg-gradient-to-r via-blue-500 from-fuchsia-600 to-blue-500 bg-clip-text'>
            A look at Chris&apos; life{' '}
@@ -59,23 +60,27 @@ export default function SlideShow()
     </h1>
     <div
       ref={slideRef}
-      className="relative flex justify-center"
+      className="relative flex justify-center w-full"
       onMouseEnter={() => setIsPaused(true)}
       onMouseLeave={() => setIsPaused(false)}
     >
       <Image
         src={photos[currentImageIndex].photoLink} 
         alt={photos[currentImageIndex].description} 
-        className="rounded-xl"
+        className="aspect-video rounded-xl"
         width={1024}
         height={1024}
       />
-      <div className="absolute flex items-center justify-around w-full px-3 -bottom-12 transform -translate-y-1/2">
-        <button onClick={showPrevImage}>Prev</button>
-        <div>
+      <div className="absolute px-3 place-items-center grid-cols-3 grid -bottom-10">
+        <button onClick={showPrevImage}>
+          <BsArrowLeft className="text-3xl hover:fill-blue-500 hover:scale-105" />
+        </button>
+        <div className="mx-3 text-xl">
           <p>{currentImageIndex === 0 ? "00" : "0"}{(currentImageIndex + 1).toString(2)} : {photos.length}</p>
         </div>
-        <button onClick={showNextImage}>Next</button>
+        <button onClick={showNextImage}>
+          <BsArrowRight className="text-3xl hover:fill-blue-500 hover:scale-105" />
+        </button>
       </div>
     </div>
   </div>)
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,7 +18,7 @@ export default function Page() {
   const [showWorld, setShowWorld] = useState(false)
   return (
     <>
-      <section className='flex flex-col items-center order-1 w-full min-h-0 bg-gradient-to-b from-black'>
+      <section className='flex flex-col items-center w-full min-h-0 bg-gradient-to-b from-black'>
         <div className='flex flex-col items-center w-full h-screen md:w-11/12 lg:w-10/12 xl:w-9/12'>
           <motion.div
             initial={{ opacity: 0 }}
@@ -71,19 +71,19 @@ export default function Page() {
 
       {/*------------- SECTION 2 ------------ SECTION 2 ------------*/}
 
-      <section className='relative flex flex-col items-center justify-around order-2 h-max col-span-full'>
+      <section className='relative flex flex-col items-center justify-around h-max col-span-full'>
         <SkillList />
       </section>
 
       {/*------------- SECTION 3 ------------ SECTION 3 ---------------*/}
 
-      <section className='flex flex-col items-center order-3 px-2 py-10 bg-black/20 h-max col-span-full'>
+      <section className='flex flex-col items-center px-2 py-10 bg-black/20 h-max col-span-full'>
         <WorkCards />
       </section>
 
       {/*------------- SECTION 4 ------------ SECTION 4 ---------------*/}
 
-      <section className='flex flex-col items-center py-1 my-10 col-span-full'>
+      <section className='flex flex-col items-center px-2 py-10 my-10 h-max col-span-full'>
         <SlideShow />
       </section>
 
