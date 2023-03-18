a8268b53364559b6ca1e9860cd4800a0163a5fe0
Chris Ragland <Chrisragland97@gmail.com>
2023-03-18 14:32:31 -0400

    slide show working, aspect ratio of photos fix needed but that would be it

--- src/components/dom/SlideShow.jsx
+++ src/components/dom/SlideShow.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from "react"
-import Image from "next/image";
+import { useInView } from 'framer-motion'
 import { BsArrowLeft, BsArrowRight } from "react-icons/bs"
+import { AnimatePresence, motion } from "framer-motion"
 
 //array of photos
 const photos = [
@@ -9,7 +10,7 @@ const photos = [
     description: "cda lab circuit"
   },
   {
-    photoLink: '/img/work-images/vidal-developers-landing-page.webp',
+    photoLink: '/img/life-images/group-photo.webp',
     description: "test 2"
   },
   {
@@ -23,7 +24,8 @@ export default function SlideShow()
 {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [isPaused, setIsPaused] = useState(false);
-  const slideRef = useRef();
+  const headerRef = useRef();
+  const headerInView = useInView(headerRef, { once: true, margin: '-100px' })
 
   const showNextImage = () =>
   {
@@ -52,25 +54,35 @@ export default function SlideShow()
 
   return (
   <div className="flex flex-col items-center justify-center w-full h-full md:w-11/12 lg:w-10/12 xl:w-9/12 2xl:w-8/12">
-    <h1 className='w-11/12 mb-10 text-2xl sm:text-4xl'>
+    <h1 
+      ref={headerRef}
+      style={{
+        opacity: headerInView ? 1 : 0,
+        transition: 'all 2s ease-in-out',
+      }}
+      className='w-11/12 mb-10 text-2xl sm:text-4xl'>
           <span className='font-bold text-left text-transparent animate-text bg-gradient-to-r via-blue-500 from-fuchsia-600 to-blue-500 bg-clip-text'>
            A look at Chris&apos; life{' '}
           </span>
           👀
     </h1>
     <div
-      ref={slideRef}
       className="relative flex justify-center w-full"
       onMouseEnter={() => setIsPaused(true)}
       onMouseLeave={() => setIsPaused(false)}
     >
-      <Image
-        src={photos[currentImageIndex].photoLink} 
-        alt={photos[currentImageIndex].description} 
-        className="aspect-video rounded-xl"
-        width={1024}
-        height={1024}
-      />
+      <AnimatePresence mode="wait">
+        <motion.img
+          src={photos[currentImageIndex].photoLink} 
+          key={photos[currentImageIndex].photoLink}
+          alt={photos[currentImageIndex].description} 
+          className="rounded-xl"
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          transition={{duration: .62}}
+          exit={{opacity: 0}}
+        />
+      </AnimatePresence>
       <div className="absolute px-3 place-items-center grid-cols-3 grid -bottom-10">
         <button onClick={showPrevImage}>
           <BsArrowLeft className="text-3xl hover:fill-blue-500 hover:scale-105" />
