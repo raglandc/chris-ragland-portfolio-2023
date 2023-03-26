cb8f93d51968584a231f3d4d522318d101e4725d
Chris Ragland <Chrisragland97@gmail.com>
2023-03-26 11:19:55 -0400

    fix merge conflicts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,17 +3,7 @@ import { motion } from 'framer-motion'
 import Typewriter from 'typewriter-effect'
 import SkillList from '@/components/dom/SkillList'
 import WorkCards from '@/components/dom/WorkCards'
-<<<<<<< HEAD
-=======
 import SlideShow from '@/components/dom/SlideShow'
-import FadeIcon from '@/components/dom/util/FadeIcon'
-
-// Dynamic import is used to prevent a payload when the website starts, that includes threejs, r3f etc..
-// WARNING ! errors might get obfuscated by using dynamic import.
-// If something goes wrong go back to a static import to show the error.
-// https://github.com/pmndrs/react-three-next/issues/49
-const SceneContainer = dynamic(() => import('@/components/canvas/SceneContainer.jsx'))
->>>>>>> slide-show
 
 export default function Page() {
   return (
