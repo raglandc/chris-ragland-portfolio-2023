81e107959203ff34101b3de8a2ded530964aa06b
Chris Ragland <Chrisragland97@gmail.com>
2023-05-26 18:41:48 -0400

    fix offset skill

--- src/components/dom/SkillList.jsx
+++ src/components/dom/SkillList.jsx
@@ -95,7 +95,7 @@ function SkillCard({ title, logo }) {
         transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
       }}
     >
-      <div className='flex flex-col items-center justify-center px-4 py-5'>
+      <div className='flex flex-col items-center justify-center px-4 py-5 text-center'>
         <h1 className='mb-3'>{title}</h1>
         <span className='text-2xl'>{logo}</span>
       </div>
